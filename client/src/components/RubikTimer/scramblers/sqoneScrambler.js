65930d8983debbbf78756c43dcc8f26a2600b67f
Lucas <lucas.115@hotmail.com>
2025-03-08 21:02:30 +0100

    Make square1 scrambles match shape preservation and index frequency

--- client/src/components/RubikTimer/scramblers/sqoneScrambler.js
+++ client/src/components/RubikTimer/scramblers/sqoneScrambler.js
@@ -36,33 +36,43 @@ export const Scramble = () => {
     let downIdx;
     let availableIndexes;
     let state = INITIAL_STATE.map(row => row.slice());
-    for (let i = 0; i < SCRAMBLE_LENGTH - 1; i++) {
+    for (let i = 0; i < SCRAMBLE_LENGTH; i++) {
         availableIndexes = getAvailableIndexes(state);
+        if (0 !== generateRandomBetweenZeroAndX(4)) { // sixes seem less likely than they should be naturally
+            availableIndexes[0] = availableIndexes[0].filter((idx) => idx !== 6);
+            availableIndexes[1] = availableIndexes[1].filter((idx) => idx !== 6);
+        }
+        if (i > 3 && availableIndexes[1].length < 3) { // make sure down can be 0
+            availableIndexes[0].shift(1);
+        }
         upIdx = availableIndexes[0][generateRandomBetweenZeroAndX(availableIndexes[0].length)];
-        if (upIdx === 0) {
-            availableIndexes[1].shift();
+        state[0] = state[0].slice(upIdx).concat(state[0].slice(0, upIdx));
+        if (i > 2) { // no positive numbers down after third move for some reason. Seem converted to 0
+            availableIndexes[1] = availableIndexes[1].map((idx) => idx > 6 ? idx : 0);
+        }
+        if (upIdx === 0) { // remove down 0 if up is 0
+            availableIndexes[1] = availableIndexes[1].filter((idx) => idx !== 0);
+        }
+        if (i < 4) { // prevent shape change for the first four moves
+            if (state[0][0] === state[1][0]) {
+                availableIndexes[1] = availableIndexes[1].filter((idx) => idx % 3 === 0);
+            } else {
+                availableIndexes[1] = availableIndexes[1].filter((idx) => idx % 3 !== 0);
+            }
         }
         downIdx = availableIndexes[1][generateRandomBetweenZeroAndX(availableIndexes[1].length)];
-        state[0] = state[0].slice(upIdx).concat(state[0].slice(0, upIdx));
         state[1] = state[1].slice(downIdx).concat(state[1].slice(0, downIdx));
         slice(state);
         scramble += "(".concat(mapIdxToMove(upIdx))
             .concat(",")
             .concat(mapIdxToMove(downIdx, true))
-            .concat(")/ ");
-        if (0 === (i + 1) % 3) {
+            .concat(")");
+        if (i !== SCRAMBLE_LENGTH - 1 || 0 === generateRandomBetweenZeroAndX(3)) {
+            scramble += "/ ";
+        }
+        if (0 === (i + 1) % 3 && i !== SCRAMBLE_LENGTH - 1) {
             scramble += "\n";
         }
-    }
-    availableIndexes = getAvailableIndexes(state);
-    upIdx = availableIndexes[0][generateRandomBetweenZeroAndX(availableIndexes[0].length)];
-    downIdx = availableIndexes[1][generateRandomBetweenZeroAndX(availableIndexes[1].length)];
-    scramble += "(".concat(mapIdxToMove(upIdx))
-        .concat(",")
-        .concat(mapIdxToMove(downIdx, true))
-        .concat(")");
-    if (0 === generateRandomBetweenZeroAndX(3)) {
-        scramble = scramble.concat("/ ");
     }
     return scramble;
 }
