09fb9179c59e5fa4a604bf176a961f0648c0c34b
Lucas <lucas.115@hotmail.com>
2025-04-24 18:35:47 +0200

    Configure security

--- server/src/main/java/com/lucas/server/config/SecurityConfig.java
+++ server/src/main/java/com/lucas/server/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -39,16 +40,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, CorsFilter corsFilter,
                                                    RateLimitFilter rateLimitingFilter) throws Exception {
-        http.csrf()
-                .disable()
-                .authorizeHttpRequests()
-                .anyRequest().authenticated()
-                .and()
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
                 .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(rateLimitingFilter, UsernamePasswordAuthenticationFilter.class)
-                .httpBasic()
-                .and()
-                .formLogin();
+                .httpBasic(basic -> {
+                })
+                .formLogin(form -> {
+                });
+
         return http.build();
     }
 }
