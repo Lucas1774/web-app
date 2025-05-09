54cff35f4d1764922e1a6fc354a32a7c57f8c8cd
Lucas <lucas.115@hotmail.com>
2025-05-10 00:43:55 +0200

    Correctly pick AzureOpenAi Autoconfigured wrappers

--- server/src/main/java/com/lucas/server/components/tradingbot/news/service/NewsEmbeddingsClient.java
+++ server/src/main/java/com/lucas/server/components/tradingbot/news/service/NewsEmbeddingsClient.java
@@ -1,25 +1,17 @@
 package com.lucas.server.components.tradingbot.news.service;
 
-import com.azure.ai.openai.OpenAIClient;
-import com.azure.ai.openai.models.EmbeddingsOptions;
 import com.lucas.server.common.exception.ClientException;
 import com.lucas.server.components.tradingbot.news.jpa.News;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ai.azure.openai.AzureOpenAiEmbeddingModel;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
-import java.util.stream.IntStream;
 
 @Component
 public class NewsEmbeddingsClient {
 
-    private final OpenAIClient client;
-    private final String model;
+    private final AzureOpenAiEmbeddingModel client;
 
-    public NewsEmbeddingsClient(OpenAIClient client,
-                                @Value("${spring.ai.azure.openai.embeddings.options.deployment-name}") String model) {
+    public NewsEmbeddingsClient(AzureOpenAiEmbeddingModel client) {
         this.client = client;
-        this.model = model;
     }
 
     public News embed(News news) throws ClientException {
@@ -27,22 +19,9 @@ public class NewsEmbeddingsClient {
         if (news.getSummary() != null && !news.getSummary().isEmpty()) {
             contentToEmbed += " " + news.getSummary();
         }
-        EmbeddingsOptions options = new EmbeddingsOptions(List.of(contentToEmbed))
-                .setModel(model);
 
         try {
-            float[] embeddings = client.getEmbeddings(model, options)
-                    .getData()
-                    .stream()
-                    .findFirst()
-                    .map(item -> {
-                        List<Float> vector = item.getEmbedding();
-                        float[] array = new float[vector.size()];
-                        IntStream.range(0, vector.size())
-                                .forEach(i -> array[i] = vector.get(i));
-                        return array;
-                    })
-                    .orElse(new float[0]);
+            float[] embeddings = client.embed(contentToEmbed);
             if (embeddings.length > 0) {
                 Thread.sleep(3000);
             }
--- server/src/main/java/com/lucas/server/config/OpenAIConfig.java
+++ server/src/main/java/com/lucas/server/config/OpenAIConfig.java
@@ -1,21 +1,38 @@
 package com.lucas.server.config;
 
-import com.azure.ai.openai.OpenAIClient;
 import com.azure.ai.openai.OpenAIClientBuilder;
 import com.azure.core.http.policy.ExponentialBackoffOptions;
 import com.azure.core.http.policy.RetryOptions;
+import com.azure.core.http.policy.RetryPolicy;
+import io.micrometer.observation.ObservationRegistry;
+import org.springframework.ai.azure.openai.AzureOpenAiEmbeddingModel;
+import org.springframework.ai.embedding.observation.EmbeddingModelObservationConvention;
+import org.springframework.ai.model.azure.openai.autoconfigure.AzureOpenAiEmbeddingProperties;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * BIG hacks to inject a non-bugged retry policy into the openAIClients
+ */
 @Configuration
 public class OpenAIConfig {
 
     @Bean
-    public OpenAIClient openAIClient(OpenAIClientBuilder builder) {
-        ExponentialBackoffOptions exponentialBackoffOptions = new ExponentialBackoffOptions();
-        exponentialBackoffOptions.setMaxRetries(0);
-        RetryOptions retryOptions = new RetryOptions(exponentialBackoffOptions);
+    public RetryPolicy retryPolicy() {
+        return new RetryPolicy(new RetryOptions(new ExponentialBackoffOptions().setMaxRetries(0)));
+    }
 
-        return builder.retryOptions(retryOptions).buildClient();
+    @Bean
+    public AzureOpenAiEmbeddingModel azureOpenAiEmbeddingModel(RetryPolicy retryPolicy, OpenAIClientBuilder openAIClientBuilder,
+                                                               AzureOpenAiEmbeddingProperties embeddingProperties, ObjectProvider<ObservationRegistry> observationRegistry,
+                                                               ObjectProvider<EmbeddingModelObservationConvention> observationConvention) {
+        var embeddingModel = new AzureOpenAiEmbeddingModel(openAIClientBuilder.retryPolicy(retryPolicy).buildClient(),
+                embeddingProperties.getMetadataMode(), embeddingProperties.getOptions(),
+                observationRegistry.getIfUnique(() -> ObservationRegistry.NOOP));
+
+        observationConvention.ifAvailable(embeddingModel::setObservationConvention);
+
+        return embeddingModel;
     }
 }
