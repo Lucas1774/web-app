38954ffb7cf7ff7b5544844109b764e59569a057
Lucas <lucas.115@hotmail.com>
2025-06-11 21:36:13 +0200

    Configure more models but try to greed using the better ones and tweak prompt

--- server/src/main/java/com/lucas/server/common/Constants.java
+++ server/src/main/java/com/lucas/server/common/Constants.java
@@ -19,21 +19,32 @@ public class Constants {
 
     public enum Clients {
         GPT_4_1("gpt-4.1"),
-        GPT_4O("gpt-4o"),
-        DEEPSEEK_V3("deepseek-v3"),
-        GROK_3("grok-3"),
         GPT_4_1_MINI("gpt-4.1-mini"),
         GPT_4_1_NANO("gpt-4.1-nano"),
+        GPT_4O("gpt-4o"),
+        GPT_4O_MINI("gpt-4o-mini"),
+        DEEPSEEK_V3("deepseek-v3"),
+        GROK_3("grok-3"),
         LLAMA_3_3_70B("llama-3.3-70b"),
         LLAMA_3_1_405B("llama-3.1-405b"),
         LLAMA_3_1_70B("llama-3.1-70b"),
+        LLAMA_3_1_8B("llama-3.1-8b"),
         MISTRAL_LARGE("mistral-large"),
-        GPT_4O_MINI("gpt-4o-mini"),
         MISTRAL_NEMO("mistral-nemo"),
         MINISTRAL_3B("ministral-3b"),
+        COMMAND_R_PLUS_2024("command-r-plus-2024"),
+        COMMAND_A("command-a"),
+        A21_JAMBA("a21-jamba"),
         PHI_4("phi-4"),
-        LLAMA_3_1_8B("llama-3.1-8b"),
-        COMMAND_A("command-a");
+        DEEPSEEK_R1_0528("deepseek-r1-0528"), // not working
+        DEEPSEEK_R1("deepseek-r1"), // not working
+        COMMAND_R_PLUS("command-r-plus"), // not working
+        O4_MINI("o4-mini"), // not working
+        O3("o3"), // not working
+        O3_MINI("o3-mini"), // not working
+        O1("o1"), // not working
+        O1_PREVIEW("o1-preview"), // not working
+        O1_MINI("o1-mini"); // not working
 
         private final String label;
 
@@ -104,9 +115,8 @@ public class Constants {
     public static final String INSUFFICIENT_STOCK_ERROR = "{0}: Nothing to sell";
     public static final String SYMBOL_NOT_FOUND_ERROR = "{0}: Unknown symbol";
 
-    public static final List<Clients> RECOMMENDATION_CLIENTS = List.of(GPT_4_1, DEEPSEEK_V3, GROK_3, GPT_4O);
-    public static final List<Clients> RANDOM_RECOMMENDATION_CLIENTS = List.of(LLAMA_3_3_70B, LLAMA_3_1_405B, LLAMA_3_1_70B,
-            MISTRAL_LARGE, GPT_4_1_MINI, PHI_4, GPT_4O_MINI, MISTRAL_NEMO, LLAMA_3_1_8B, MINISTRAL_3B, GPT_4_1_NANO, COMMAND_A);
+    public static final List<Clients> RECOMMENDATION_CLIENTS = List.of(GPT_4_1, GPT_4O, GROK_3, DEEPSEEK_V3, LLAMA_3_3_70B, LLAMA_3_1_405B);
+    public static final List<Clients> RANDOM_RECOMMENDATION_CLIENTS = List.of(GPT_4_1, GPT_4O, GROK_3, DEEPSEEK_V3, LLAMA_3_3_70B, LLAMA_3_1_405B);
 
     public static final List<String> SP500_SYMBOLS = List.of("MMM", "AOS", "ABT", "ABBV", "ACN", "ADBE", "AMD", "AES", "AFL", "A", "APD", "ABNB", "AKAM", "ALB", "ARE", "ALGN", "ALLE", "LNT", "ALL", "GOOGL", "GOOG", "MO", "AMZN", "AMCR", "AEE", "AEP", "AXP", "AIG", "AMT", "AWK", "AMP", "AME", "AMGN", "APH", "ADI", "ANSS", "AON", "APA", "APO", "AAPL", "AMAT", "APTV", "ACGL", "ADM", "ANET", "AJG", "AIZ", "T", "ATO", "ADSK", "ADP", "AZO", "AVB", "AVY", "AXON", "BKR", "BALL", "BAC", "BAX", "BDX", "BRK.B", "BBY", "TECH", "BIIB", "BLK", "BX", "BK", "BA", "BKNG", "BSX", "BMY", "AVGO", "BR", "BRO", "BF.B", "BLDR", "BG", "BXP", "CHRW", "CDNS", "CZR", "CPT", "CPB", "COF", "CAH", "KMX", "CCL", "CARR", "CAT", "CBRE", "CDW", "COR", "CNC", "CNP", "CF", "CRL", "SCHW", "CHTR", "CVX", "CMG", "CB", "CHD", "CI", "CINF", "CTAS", "CSCO", "C", "CFG", "CLX", "CME", "CMS", "KO", "CTSH", "COIN", "CL", "CMCSA", "CAG", "COP", "ED", "STZ", "CEG", "COO", "CPRT", "GLW", "CPAY", "CTVA", "CSGP", "COST", "CTRA", "CRWD", "CCI", "CSX", "CMI", "CVS", "DHR", "DRI", "DVA", "DAY", "DECK", "DE", "DELL", "DAL", "DVN", "DXCM", "FANG", "DLR", "DG", "DLTR", "D", "DPZ", "DASH", "DOV", "DOW", "DHI", "DTE", "DUK", "DD", "EMN", "ETN", "EBAY", "ECL", "EIX", "EW", "EA", "ELV", "EMR", "ENPH", "ETR", "EOG", "EPAM", "EQT", "EFX", "EQIX", "EQR", "ERIE", "ESS", "EL", "EG", "EVRG", "ES", "EXC", "EXE", "EXPE", "EXPD", "EXR", "XOM", "FFIV", "FDS", "FICO", "FAST", "FRT", "FDX", "FIS", "FITB", "FSLR", "FE", "FI", "F", "FTNT", "FTV", "FOXA", "FOX", "BEN", "FCX", "GRMN", "IT", "GE", "GEHC", "GEV", "GEN", "GNRC", "GD", "GIS", "GM", "GPC", "GILD", "GPN", "GL", "GDDY", "GS", "HAL", "HIG", "HAS", "HCA", "DOC", "HSIC", "HSY", "HES", "HPE", "HLT", "HOLX", "HD", "HON", "HRL", "HST", "HWM", "HPQ", "HUBB", "HUM", "HBAN", "HII", "IBM", "IEX", "IDXX", "ITW", "INCY", "IR", "PODD", "INTC", "ICE", "IFF", "IP", "IPG", "INTU", "ISRG", "IVZ", "INVH", "IQV", "IRM", "JBHT", "JBL", "JKHY", "J", "JNJ", "JCI", "JPM", "JNPR", "K", "KVUE", "KDP", "KEY", "KEYS", "KMB", "KIM", "KMI", "KKR", "KLAC", "KHC", "KR", "LHX", "LH", "LRCX", "LW", "LVS", "LDOS", "LEN", "LII", "LLY", "LIN", "LYV", "LKQ", "LMT", "L", "LOW", "LULU", "LYB", "MTB", "MPC", "MKTX", "MAR", "MMC", "MLM", "MAS", "MA", "MTCH", "MKC", "MCD", "MCK", "MDT", "MRK", "META", "MET", "MTD", "MGM", "MCHP", "MU", "MSFT", "MAA", "MRNA", "MHK", "MOH", "TAP", "MDLZ", "MPWR", "MNST", "MCO", "MS", "MOS", "MSI", "MSCI", "NDAQ", "NTAP", "NFLX", "NEM", "NWSA", "NWS", "NEE", "NKE", "NI", "NDSN", "NSC", "NTRS", "NOC", "NCLH", "NRG", "NUE", "NVDA", "NVR", "NXPI", "ORLY", "OXY", "ODFL", "OMC", "ON", "OKE", "ORCL", "OTIS", "PCAR", "PKG", "PLTR", "PANW", "PARA", "PH", "PAYX", "PAYC", "PYPL", "PNR", "PEP", "PFE", "PCG", "PM", "PSX", "PNW", "PNC", "POOL", "PPG", "PPL", "PFG", "PG", "PGR", "PLD", "PRU", "PEG", "PTC", "PSA", "PHM", "PWR", "QCOM", "DGX", "RL", "RJF", "RTX", "O", "REG", "REGN", "RF", "RSG", "RMD", "RVTY", "ROK", "ROL", "ROP", "ROST", "RCL", "SPGI", "CRM", "SBAC", "SLB", "STX", "SRE", "NOW", "SHW", "SPG", "SWKS", "SJM", "SW", "SNA", "SOLV", "SO", "LUV", "SWK", "SBUX", "STT", "STLD", "STE", "SYK", "SMCI", "SYF", "SNPS", "SYY", "TMUS", "TROW", "TTWO", "TPR", "TRGP", "TGT", "TEL", "TDY", "TER", "TSLA", "TXN", "TPL", "TXT", "TMO", "TJX", "TKO", "TSCO", "TT", "TDG", "TRV", "TRMB", "TFC", "TYL", "TSN", "USB", "UBER", "UDR", "ULTA", "UNP", "UAL", "UPS", "URI", "UNH", "UHS", "VLO", "VTR", "VLTO", "VRSN", "VRSK", "VZ", "VRTX", "VTRS", "VICI", "V", "VST", "VMC", "WRB", "GWW", "WAB", "WBA", "WMT", "DIS", "WBD", "WM", "WAT", "WEC", "WFC", "WELL", "WST", "WDC", "WY", "WSM", "WMB", "WTW", "WDAY", "WYNN", "XEL", "XYL", "YUM", "ZBRA", "ZBH", "ZTS");
 }
